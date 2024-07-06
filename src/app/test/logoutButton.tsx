b1429132caddedf82baf184018c587d4e2609e23
Nathania Calista <91357043+nathaniacalista01@users.noreply.github.com>
2024-07-06 23:54:28 +0700

    Fix: code style

--- src/app/test/logoutButton.tsx
+++ src/app/test/logoutButton.tsx
@@ -1,12 +1,12 @@
-"use client";
-import { signOut } from "next-auth/react";
-import { useRouter } from "next/navigation";
+'use client';
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const LogoutButton = () => {
   const router = useRouter();
   const handleLogout = async () => {
     await signOut();
-    router.push("/test/login");
+    router.push('/test/login');
   };
 
   return <button onClick={handleLogout}>Logout</button>;
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,8 +1,8 @@
 // import { postRouter } from "~/server/api/routers/post";
-import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { createCallerFactory, createTRPCRouter } from '~/server/api/trpc';
 // import { userRouter } from "./routers/user";
-import { profileRouter } from "./routers/profile";
-import { userRouter } from "./routers/user";
+import { profileRouter } from './routers/profile';
+import { userRouter } from './routers/user';
 /**
  * This is the primary router for your server.
  *
