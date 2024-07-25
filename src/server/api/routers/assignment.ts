9e80828c2d861d5b97c3512697f623414c20498e
HoseaNA <13521057@std.stei.itb.ac.id>
2024-07-25 13:06:05 +0700

    feat: get daily and side quest

---
+++ src/server/api/routers/assignment.ts
@@ -0,0 +1,56 @@
+import { createTRPCRouter, publicProcedure } from '../trpc';
+import { assignmentTypeEnum, assignments } from '@katitb2024/database';
+import { TRPCError } from '@trpc/server';
+import { eq } from 'drizzle-orm';
+
+export const assignmentRouter = createTRPCRouter({
+  getDailyQuest: publicProcedure.query(async ({ ctx }) => {
+    try {
+      const dailyQuests = await ctx.db
+        .select()
+        .from(assignments)
+        .where(
+          eq(assignments.assignmentType, assignmentTypeEnum.enumValues[0]),
+        );
+
+      if (!dailyQuests) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'No daily quest found',
+        });
+      }
+
+      return dailyQuests;
+    } catch (e) {
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: 'Internal server error',
+      });
+    }
+  }),
+
+  getSideQuest: publicProcedure.query(async ({ ctx }) => {
+    try {
+      const sideQuests = await ctx.db
+        .select()
+        .from(assignments)
+        .where(
+          eq(assignments.assignmentType, assignmentTypeEnum.enumValues[1]),
+        );
+
+      if (!sideQuests) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'No side quest found',
+        });
+      }
+
+      return sideQuests;
+    } catch (e) {
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: 'Internal server error',
+      });
+    }
+  }),
+});
