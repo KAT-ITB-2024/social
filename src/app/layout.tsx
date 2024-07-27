2ab7ccffacff349d771119682f64cf962dd6e22c
Nathania Calista <91357043+nathaniacalista01@users.noreply.github.com>
2024-07-27 12:47:35 +0700

    Feat: message event FE

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import '~/styles/globals.css';
 import { GeistSans } from 'geist/font/sans';
 
 import { TRPCReactProvider } from '~/trpc/react';
+import { SessionProvider } from 'next-auth/react';
+import { NextAuthProvider } from './provider';
 
 export const metadata = {
   title: 'Create T3 App',
@@ -18,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <TRPCReactProvider>
+          <NextAuthProvider>{children}</NextAuthProvider>
+        </TRPCReactProvider>
       </body>
     </html>
   );
--- src/server/socket/events/message.ts
+++ src/server/socket/events/message.ts
@@ -7,53 +7,28 @@ export const messageEvent = createEvent(
     name: 'message',
     input: z.object({
       message: z.string().min(1),
-      receiverId: z.string().min(1),
     }),
     authRequired: true,
   },
   async ({ ctx, input }) => {
+    console.log('MASUK KE MESSAGE EVENT');
     const userSession = ctx.client.data.session;
     const senderId = userSession.user.id;
-    const receiverId = input.receiverId;
-
-    let roomChat =
-      ctx.client.data.roomChat.get(receiverId) ??
-      (await ctx.drizzle.query.userMatches.findFirst({
-        where: or(
-          and(
-            eq(userMatches.firstUserId, senderId),
-            eq(userMatches.secondUserId, receiverId),
-          ),
-          and(
-            eq(userMatches.secondUserId, senderId),
-            eq(userMatches.firstUserId, receiverId),
-          ),
-        ),
-      }));
-
-    if (!roomChat) {
-      const result = await ctx.drizzle
-        .insert(userMatches)
-        .values({
-          firstUserId: senderId,
-          secondUserId: receiverId,
-          topic: '',
-        })
-        .returning();
-      if (result[0]) {
-        roomChat = result[0];
-        ctx.client.data.roomChat.set(receiverId, roomChat);
-      } else {
-        return;
-      }
+    const currentMatch = ctx.client.data.match;
+    if (!currentMatch) {
+      return;
     }
+    const receiverId =
+      currentMatch.firstUserId === senderId
+        ? currentMatch.secondUserId
+        : currentMatch.firstUserId;
 
     const insertResult = await ctx.drizzle
       .insert(messages)
       .values({
         senderId: senderId,
         receiverId: receiverId,
-        userMatchId: roomChat.id,
+        userMatchId: currentMatch.id,
         content: input.message,
       })
       .returning();
