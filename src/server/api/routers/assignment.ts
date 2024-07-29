776be3da22d9d8c54927df73747568aa7a744581
HoseaNA <13521057@std.stei.itb.ac.id>
2024-07-29 13:26:19 +0700

    feat: get assignment by assignment id

--- src/server/api/routers/assignment.ts
+++ src/server/api/routers/assignment.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter, publicProcedure } from '../trpc';
 import { assignmentTypeEnum, assignments } from '@katitb2024/database';
 import { TRPCError } from '@trpc/server';
 import { eq } from 'drizzle-orm';
+import { getAssignmentByIdPayload } from '~/types/payloads/assignment';
 
 export const assignmentRouter = createTRPCRouter({
   getDailyQuest: publicProcedure.query(async ({ ctx }) => {
@@ -53,4 +54,34 @@ export const assignmentRouter = createTRPCRouter({
       });
     }
   }),
+
+  getQuestById: publicProcedure
+    .input(getAssignmentByIdPayload)
+    .query(async ({ ctx, input }) => {
+      try {
+        if (!input.id)
+          throw new TRPCError({
+            code: 'BAD_REQUEST',
+            message: 'Assignment ID was not recieved',
+          });
+
+        const quest = await ctx.db
+          .select()
+          .from(assignments)
+          .where(eq(assignments.id, input.id));
+
+        if (!quest)
+          throw new TRPCError({
+            code: 'NOT_FOUND',
+            message: 'Quest not found',
+          });
+
+        return quest;
+      } catch (e) {
+        throw new TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: 'Internal server error',
+        });
+      }
+    }),
 });
