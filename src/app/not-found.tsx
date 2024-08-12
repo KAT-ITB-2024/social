0a13c9b13aaf09e5ac00a025e61d975762440f0e
hiirrs <13522085@std.stei.itb.ac.id>
2024-08-12 20:01:06 +0700

    feat: add ComingSoon and Maintenance components, add not-found.tsx page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,26 +7,29 @@ export default function Home() {
       <div
         className="fixed-width-container flex flex-col items-center justify-center"
         style={{
-          backgroundImage: "url('/components/bg-404.svg')",
+          backgroundImage: "url('/components/bg-404.png')",
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover',
         }}
       >
         <Image
           className="-mb-8"
-          src="/components/404.svg"
+          src="/components/404.png"
           alt="404 | Page not found"
           width={300}
           height={300}
         />
         <p className="text-blue-600 text-center text-5xl px-10 font-heading ">
-          UPS!
+          {' '}
+          UPS!{' '}
         </p>
         <p className="text-blue-600 text-center text-xl px-10 font-bold">
-          Kamu menyelam terlalu dalam!
+          {' '}
+          Kamu menyelam terlalu dalam!{' '}
         </p>
         <p className="text-blue-600 text-center text-base px-10">
-          Jangan khawatir, Aqualings! Kalian bisa
+          {' '}
+          Jangan khawatir, Aqualings! Kalian bisa{' '}
         </p>
         <p className="text-blue-600 text-center text-base px-10">
           langsung kembali ke <span> </span>
