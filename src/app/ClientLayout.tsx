261e9be3ce6e9987e3811ae0681e9a36646057fb
Alexander Jason <alexanderjason526@gmail.com>
2024-08-12 13:35:58 +0700

    fix: update navbar positioning

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -8,7 +8,7 @@ export default function ClientLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen max-w-md mx-auto flex flex-col">
+    <div className="min-h-screen max-w-md mx-auto flex flex-col w-full">
       <Navbar />
       <div className="absolute max-w-md flex-grow top-0 mx-auto w-full">
         {children}
