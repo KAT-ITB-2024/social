9d67a2f91c9a073ed467419cbbd117e99ec896b3
AtqiyaHaydar <ashford1524@gmail.com>
2024-08-12 13:36:26 +0700

    feat: add forgot pass page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,67 +1,86 @@
+"use client"
+
 import React from 'react';
 
+// Images Import
 import Image from 'next/image';
-import LoginBackground from 'public/login/Login Background.png';
 import OSKMLogo from 'public/login/Logo.png';
-import BubbleCorner from 'public/login/Bubble Corner.png';
-import Bubble from 'public/login/Bubble.png';
-import CoralPensu from 'public/login/Coral Pensu.png';
-import Coral from 'public/login/Coral.png';
-import Fish from 'public/login/Fish.png';
-import SeaHorse from 'public/login/Sea Horse.png';
-import Starfish from 'public/login/Starfish.png';
+
+// Form Import
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { loginPayload } from '~/types/payloads/login';
+import { useForm } from 'react-hook-form';
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import Link from 'next/link';
 
 const LoginPage = () => {
+  type loginPayloadSchema = z.infer<typeof loginPayload>
+
+  const form = useForm<loginPayloadSchema>({
+    resolver: zodResolver(loginPayload),
+    defaultValues: {
+      nim: "",
+      password: ""
+    }
+  })
+
+  function onSubmit(values: loginPayloadSchema) {
+    console.log(values)
+  }
+
   return (
-    <div className="bg-black h-screen w-full flex items-center justify-center">
-      {/* Iphone View : Ukuran Width mengikuti Figma, Ukuran Height Screen */}
-      <div className="relative h-full w-[393px] bg-white overflow-hidden">
-        {/* Background */}
-        <Image
-          src={LoginBackground}
-          alt="OSKM Login Page Background"
-          className="absolute top-0 left-0 object-cover h-full w-full -z-1"
-        />
-
-        {/* Bottom Ornament */}
-        <Image
-          src={CoralPensu}
-          alt="Coral Pensu"
-          className="absolute bottom-[-70px] left-[-100px] object-cover z-10"
-          width={350}
-          height={350}
-        />
-        <Image
-          src={Coral}
-          alt="Coral"
-          className="absolute bottom-0 right-0 object-cover"
-          width={300}
-          height={300}
-        />
-
-        {/* Top Ornament */}
-        <Image
-          src={BubbleCorner}
-          alt="Bubble in the Corner"
-          className="absolute top-[-50px] right-[-10px]"
-          width={100}
-          height={100}
-        />
-        <Image
-          src={Starfish}
-          alt="Starfish"
-          className="absolute top-[20px] right-[90px]"
-          width={80}
-          height={80}
-        />
-        <Image
-          src={SeaHorse}
-          alt="Sea Horse"
-          className="absolute top-[70px] right-[50px]"
-          width={70}
-          height={70}
-        />
-      </div>
+    <div className='mt-[120px] w-full flex flex-col items-center'>
+      <Image 
+        src={OSKMLogo}
+        alt='Logo OSKM'
+        width={100}
+        height={100}
+      />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2 z-20 w-full">
+          <FormField 
+            control={form.control}
+            name='nim'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className='text-blue-500'>NIM <span className='text-red-500'>*</span></FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder='NIM' className='focus-visible:ring-transparent border-neutral-400 rounded-lg border-2'/> 
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField 
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className='text-blue-500'>Password <span className='text-red-500'>*</span></FormLabel>
+                <FormControl>
+                  <Input {...field} type="password" placeholder='Password' className='focus-visible:ring-transparent border-neutral-400 rounded-lg border-2'/>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Link href="/login/forgot-password" className='underline text-start text-[12px] text-blue-500'>Lupa Password?</Link>
+          <div className='py-2' />
+          <div className='w-full flex justify-center'>
+            <Button type="submit" className=" bg-blue-500 hover:bg-blue-400 shadow-lg px-8">Login</Button>
+          </div>
+        </form>
+      </Form>
     </div>
   );
 };
