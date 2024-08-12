8709509107728b6cfd47486e0511debedc00bde4
Alexander Jason <alexanderjason526@gmail.com>
2024-08-12 15:29:22 +0700

    feat; add shadow overlay

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,47 +11,55 @@ const Navbar = () => {
 
   return (
     <div className="relative">
-      <div className="max-w-md w-full justify-center">
-        <div
-          className="relative bg-blue-600 py-3 pl-3 pr-5 mx-6 flex justify-between items-center rounded-full shadow-custom absolute top-4 z-20"
-          style={{
-            backgroundImage: "url('/images/navbar/seaweed.png')",
-            backgroundPosition: '95% 30%',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: '75%',
-          }}
-        >
-          <div className="flex items-center">
-            <Image
-              src="/images/oskm-logo.png"
-              alt="OSKM ITB 2024"
-              width={110}
-              height={40}
-            />
-          </div>
-          <div className="flex items-center space-x-4">
-            <button className="bg-turquoise-100 p-1 rounded-lg border-2 border-blue-600 shadow-custom">
+      {!isSidebarOpen && (
+        <div className="max-w-md w-full justify-center">
+          <div
+            className="relative bg-blue-600 py-3 pl-3 pr-5 mx-6 flex justify-between items-center rounded-full shadow-custom absolute top-4 z-20"
+            style={{
+              backgroundImage: "url('/images/navbar/seaweed.png')",
+              backgroundPosition: '95% 30%',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: '75%',
+            }}
+          >
+            <div className="flex items-center">
               <Image
-                src="/icons/notification-icon.svg"
-                alt="Notification"
-                width={24}
-                height={24}
+                src="/images/oskm-logo.png"
+                alt="OSKM ITB 2024"
+                width={110}
+                height={40}
               />
-            </button>
-            <button
-              className="bg-turquoise-100 px-[7px] py-[10px] rounded-lg border-2 border-blue-600 shadow-custom"
-              onClick={handleToggleSidebar}
-            >
-              <Image
-                src="/icons/hamburg-icon.svg"
-                alt="Menu"
-                width={18}
-                height={18}
-              />
-            </button>
+            </div>
+            <div className="flex items-center space-x-4">
+              <button className="bg-turquoise-100 p-1 rounded-lg border-2 border-blue-600 shadow-custom">
+                <Image
+                  src="/icons/notification-icon.svg"
+                  alt="Notification"
+                  width={24}
+                  height={24}
+                />
+              </button>
+              <button
+                className="bg-turquoise-100 px-[7px] py-[10px] rounded-lg border-2 border-blue-600 shadow-custom"
+                onClick={handleToggleSidebar}
+              >
+                <Image
+                  src="/icons/hamburg-icon.svg"
+                  alt="Menu"
+                  width={18}
+                  height={18}
+                />
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
+      {isSidebarOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-10"
+          onClick={handleToggleSidebar}
+        ></div>
+      )}
       <Sidebar isOpen={isSidebarOpen} toggleSidebar={handleToggleSidebar} />
     </div>
   );
