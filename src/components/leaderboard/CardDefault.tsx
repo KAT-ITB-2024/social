bcaba2aef0d15224b7eb47d84d94004f866646be
sotul04 <109208105+sotul04@users.noreply.github.com>
2024-08-15 19:32:58 +0700

    fix: fe-leaderboard add variant for card

--- src/components/leaderboard/CardDefault.tsx
+++ src/components/leaderboard/CardDefault.tsx
@@ -2,11 +2,12 @@ import Image from 'next/image';
 
 interface CardDefaultProps {
   rank: number;
-  profilePicture: string;
+  profilePicture?: string;
   name: string;
-  sid: string;
+  sid?: string;
   point: number;
-  isBelong?: boolean;
+  isUser?: boolean;
+  isIndividual?: boolean;
 }
 
 export default function CardDefault({
@@ -15,12 +16,13 @@ export default function CardDefault({
   name,
   sid,
   point,
-  isBelong = false,
+  isUser = false,
+  isIndividual = true,
 }: CardDefaultProps) {
   return (
     <div
-      className={`flex flex-row w-[345px] items-center p-2 rounded-[12px] border-[2px] border-[#05A798] gap-2 text-[#006E6F] bg-gradient-to-br ${
-        isBelong
+      className={`flex flex-row w-[345px] items-center p-2 rounded-[12px] border-[2px] border-[#05A798] gap-2 text-[#006E6F] shadow-[4px_4px_10px_0_#FFBF5180] bg-gradient-to-br ${
+        isUser
           ? 'from-[#0CEBCC99] to-[#05A798]'
           : 'from-[#C5FFF3B2] to-[#99E0FFB2]'
       }`}
@@ -32,17 +34,28 @@ export default function CardDefault({
 
       {/* Profile section*/}
       <div className="flex gap-2 flex-row items-center flex-grow">
-        <Image
-          className="w-[40px] h-[40px] rounded-full"
-          src={profilePicture}
-          alt="profile-image"
-          width={40}
-          height={40}
-        />
-        <div className="flex flex-col justify-between">
-          <p className="font-subheading font-bold">{name}</p>
-          <p className="font-body text-sm font-normal">{sid}</p>
-        </div>
+        {isIndividual && profilePicture ? (
+          <>
+            <Image
+              className="w-[40px] h-[40px] rounded-full"
+              src={profilePicture}
+              alt="profile-image"
+              width={40}
+              height={40}
+            />
+            <div className="flex flex-col justify-between">
+              <p className="font-subheading font-bold">{name}</p>
+              <p className="font-body text-sm font-normal">{sid}</p>
+            </div>
+          </>
+        ) : (
+          <>
+            <span className="w-10 flex items-center justify-center h-10 rounded-full bg-[#FEFDA3] font-heading font-normal text-xl">
+              {name}
+            </span>
+            <p className="font-subheading font-bold">{`Kelompok ${name}`}</p>
+          </>
+        )}
       </div>
 
       {/* Points */}
--- src/components/leaderboard/CardTopScore.tsx
+++ src/components/leaderboard/CardTopScore.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image';
 
-interface CardDefaultProps {
+interface CardTopScoreProps {
   rank: number;
-  profilePicture: string;
+  profilePicture?: string;
   name: string;
-  sid: string;
+  sid?: string;
   point: number;
-  isBelong?: boolean;
+  isIndividual?: boolean;
 }
 
 export default function CardTopScore({
@@ -15,30 +15,45 @@ export default function CardTopScore({
   name,
   sid,
   point,
-}: CardDefaultProps) {
+  isIndividual = true,
+}: CardTopScoreProps) {
   return (
-    <div className="flex flex-col w-[110px] items-center justify-center p-2 rounded-[12px] border-2 border-[#05A798] gap-3 text-[#006E6F] bg-gradient-to-br from-[#C5FFF3B2] to-[#99E0FFB2]">
+    <div className="flex flex-col w-[110px] items-center justify-center p-2 rounded-[12px] border-2 border-[#05A798] gap-3 text-[#006E6F] shadow-[4px_4px_10px_0_#FFBF5180] bg-gradient-to-br from-[#C5FFF3B2] to-[#99E0FFB2]">
       {/* Rank and Profile*/}
       <div className="relative mt-1">
         <span className="absolute flex items-center justify-center rounded-full z-[2] -top-1 -left-4 font-heading w-8 h-8 bg-[#006E6F] text-[#FEFDA3] font-normal">
           #{rank}
         </span>
-        <Image
-          className="w-[54px] h-[54px] rounded-full"
-          src={profilePicture}
-          alt="profile-image"
-          width={40}
-          height={40}
-        />
+        {profilePicture && isIndividual ? (
+          <Image
+            className="w-[54px] h-[54px] rounded-full"
+            src={profilePicture}
+            alt="profile-image"
+            width={54}
+            height={54}
+          />
+        ) : (
+          <span className="w-[54px] flex items-center justify-center h-[54px] rounded-full bg-[#FEFDA3] font-heading font-normal text-xl">
+            {name}
+          </span>
+        )}
       </div>
 
       {/* Identity */}
-      <p className="font-subheading font-bold text-center">{name}</p>
-      <p className="font-body text-sm font-normal text-center">{sid}</p>
+      {isIndividual && sid ? (
+        <>
+          <p className="font-subheading font-bold text-center">{name}</p>
+          <p className="font-body text-sm font-normal text-center">{sid}</p>
+        </>
+      ) : (
+        <>
+          <p className="font-subheading font-bold text-center">{`Kelompok ${name}`}</p>
+        </>
+      )}
 
       {/* Points */}
       <span className="rounded-full px-3 text-wrap font-heading font-normal bg-[#FEFDA3]">
-        {point}&nbsp;{point > 0 ? 'pts' : 'pt'}
+        {point}&nbsp;{point > -2 && point < 2 ? 'pt' : 'pts'}
       </span>
     </div>
   );
