be43ce27e7cdee2178ac85ee7ec469722850ce63
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-08-23 23:22:19 +0700

    refactor: move qna datas to its own file

--- src/components/mbti/FirstSection.tsx
+++ src/components/mbti/FirstSection.tsx
@@ -1,141 +1,10 @@
+import { Button } from '../ui/button';
 import Question from './Question';
-interface Answers {
-  answer: string;
-  point: {
-    mova?: number;
-    kovva?: number;
-    odra?: number;
-    sylas?: number;
-    ozirron?: number;
-  };
-}
-const questions: string[] = [
-  'Apa tipe kepribadianmu?',
-  'Apa rasa makanan favoritmu?',
-  'Apa genre musik favoritmu?',
-  'Kamu termasuk tim apa?',
-  'Apa pelajaran peminatan SMA kesukaanmu?',
-  'Kamu memiliki banyak tugas dengan tenggat 2 hari lagi, sedangkan kamu sedang merasa sangat lelah secara fisik dan mental. Apa yang akan kamu lakukan?',
-  'Kamu berhasil diterima di kampus impianmu, ITB. Namun, kamu tidak mendapatkan fakultas yang menjadi pilihan pertamamu. Apa pandanganmu terkait hal ini?',
-  'Saat ini fakultasmu sedang membutuhkan seorang ketua angkatan. Apakah kamu akan mengajukan diri menjadi seorang ketua angkatan?',
-  'Di antara pilihan bidang/divisi berikut dalam satu kepengurusan angkatan, manakah yang akan menjadi pilihan bidang/divisimu?',
-  'Apakah kamu semangat untuk OSKM ITB 2024?',
-];
-const answers: Answers[][] = [
-  [
-    {
-      answer: 'Extrovert',
-      point: {
-        mova: 5,
-        odra: 5,
-      },
-    },
-    {
-      answer: 'Ambivert',
-      point: {
-        ozirron: 5,
-      },
-    },
-    {
-      answer: 'Introvert',
-      point: {
-        sylas: 5,
-        kovva: 5,
-      },
-    },
-  ],
-  [
-    {
-      answer: 'Pedas',
-      point: {
-        kovva: 5,
-      },
-    },
-    {
-      answer: 'Manis',
-      point: {
-        mova: 5,
-        sylas: 5,
-      },
-    },
-    {
-      answer: 'Asin',
-      point: {
-        sylas: 5,
-        ozirron: 5,
-      },
-    },
-    {
-      answer: 'Asam',
-      point: {
-        ozirron: 5,
-      },
-    },
-    {
-      answer: 'Pahit',
-      point: {
-        odra: 5,
-      },
-    },
-  ],
-  [
-    {
-      answer: 'Alternative',
-      point: {
-        kovva: 5,
-      },
-    },
-    {
-      answer: 'Pop',
-      point: {
-        mova: 5,
-      },
-    },
-    {
-      answer: 'Rock',
-      point: {
-        odra: 5,
-      },
-    },
-    {
-      answer: 'Indie',
-      point: {
-        sylas: 5,
-      },
-    },
-    {
-      answer: 'Classical',
-      point: {
-        ozirron: 5,
-      },
-    },
-  ],
-  [
-    {
-      answer: 'Bubur diaduk',
-      point: {
-        sylas: 5,
-        mova: 5,
-      },
-    },
-    {
-      answer: 'Bubut tidak diaduk',
-      point: {
-        ozirron: 5,
-        kovva: 5,
-      },
-    },
-    {
-      answer: 'Bubur disedot',
-      point: {
-        odra: 5,
-      },
-    },
-  ],
-];
+import { FirstSectionAnswers, FirstSectionQuestions } from './QnA';
+
 export default function FirstSection() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center overflow-auto">
       <div className="flex flex-col items-center text-blue-600 mt-24 mx-8">
         <h2 className="text-h1 font-heading text-shadow-blue-md">
           First Section
@@ -146,41 +15,19 @@ export default function FirstSection() {
           answers. Good luck!
         </p>
         <div className="mt-5 w-full">
-          {questions.map((item, idx) => {
+          {FirstSectionQuestions.map((item, idx) => {
             return (
               <Question
                 key={idx}
-                question={questions[idx] ?? ''}
-                answers={answers[idx] ?? []}
+                question={item ?? ''}
+                answers={FirstSectionAnswers[idx] ?? []}
               />
             );
           })}
-          <Question
-            question="Apa tipe kepribadianmu?"
-            answers={[
-              {
-                answer: 'Extrovert',
-                point: {
-                  mova: 5,
-                  odra: 5,
-                },
-              },
-              {
-                answer: 'Ambivert',
-                point: {
-                  ozirron: 5,
-                },
-              },
-              {
-                answer: 'Introvert',
-                point: {
-                  sylas: 5,
-                  kovva: 5,
-                },
-              },
-            ]}
-          />
         </div>
+        <Button className="w-1/3 mb-16 bg-pink-400 py-5 font-body text-shadow-pink-sm text-[#FEFEFE] shadow-pink-sm">
+          <p className="text-b3">Submit</p>
+        </Button>
       </div>
     </div>
   );
---
+++ src/components/mbti/QnA.ts
@@ -0,0 +1,316 @@
+interface Answers {
+  answer: string;
+  point: {
+    mova?: number;
+    kovva?: number;
+    odra?: number;
+    sylas?: number;
+    ozirron?: number;
+  };
+}
+const FirstSectionQuestions: string[] = [
+  'Apa tipe kepribadianmu?',
+  'Apa rasa makanan favoritmu?',
+  'Apa genre musik favoritmu?',
+  'Kamu termasuk tim apa?',
+  'Apa pelajaran peminatan SMA kesukaanmu?',
+  'Kamu memiliki banyak tugas dengan tenggat 2 hari lagi, sedangkan kamu sedang merasa sangat lelah secara fisik dan mental. Apa yang akan kamu lakukan?',
+  'Kamu berhasil diterima di kampus impianmu, ITB. Namun, kamu tidak mendapatkan fakultas yang menjadi pilihan pertamamu. Apa pandanganmu terkait hal ini?',
+  'Saat ini fakultasmu sedang membutuhkan seorang ketua angkatan. Apakah kamu akan mengajukan diri menjadi seorang ketua angkatan?',
+  'Di antara pilihan bidang/divisi berikut dalam satu kepengurusan angkatan, manakah yang akan menjadi pilihan bidang/divisimu?',
+  'Apakah kamu semangat untuk OSKM ITB 2024?',
+];
+
+const FirstSectionAnswers: Answers[][] = [
+  [
+    {
+      answer: 'Extrovert',
+      point: {
+        mova: 5,
+        odra: 5,
+      },
+    },
+    {
+      answer: 'Ambivert',
+      point: {
+        ozirron: 5,
+      },
+    },
+    {
+      answer: 'Introvert',
+      point: {
+        sylas: 5,
+        kovva: 5,
+      },
+    },
+  ],
+  [
+    {
+      answer: 'Pedas',
+      point: {
+        kovva: 5,
+      },
+    },
+    {
+      answer: 'Manis',
+      point: {
+        mova: 5,
+        sylas: 5,
+      },
+    },
+    {
+      answer: 'Asin',
+      point: {
+        sylas: 5,
+        ozirron: 5,
+      },
+    },
+    {
+      answer: 'Asam',
+      point: {
+        ozirron: 5,
+      },
+    },
+    {
+      answer: 'Pahit',
+      point: {
+        odra: 5,
+      },
+    },
+  ],
+  [
+    {
+      answer: 'Alternative',
+      point: {
+        kovva: 5,
+      },
+    },
+    {
+      answer: 'Pop',
+      point: {
+        mova: 5,
+      },
+    },
+    {
+      answer: 'Rock',
+      point: {
+        odra: 5,
+      },
+    },
+    {
+      answer: 'Indie',
+      point: {
+        sylas: 5,
+      },
+    },
+    {
+      answer: 'Classical',
+      point: {
+        ozirron: 5,
+      },
+    },
+  ],
+  [
+    {
+      answer: 'Bubur diaduk',
+      point: {
+        sylas: 5,
+        mova: 5,
+      },
+    },
+    {
+      answer: 'Bubut tidak diaduk',
+      point: {
+        ozirron: 5,
+        kovva: 5,
+      },
+    },
+    {
+      answer: 'Bubur disedot',
+      point: {
+        odra: 5,
+      },
+    },
+  ],
+
+  [
+    {
+      answer: 'Matematika',
+      point: {
+        kovva: 5,
+      },
+    },
+    {
+      answer: 'Fisika',
+      point: {
+        kovva: 5,
+      },
+    },
+    {
+      answer: 'Kimia',
+      point: {
+        ozirron: 5,
+      },
+    },
+    {
+      answer: 'Biologi',
+      point: {
+        sylas: 5,
+      },
+    },
+    {
+      answer: 'Sosiologi',
+      point: {
+        sylas: 5,
+      },
+    },
+    {
+      answer: 'Ekonomi',
+      point: {
+        mova: 5,
+      },
+    },
+    {
+      answer: 'Geografi',
+      point: {
+        odra: 5,
+      },
+    },
+    {
+      answer: 'Sejarah',
+      point: {
+        ozirron: 5,
+      },
+    },
+  ],
+  [
+    {
+      answer:
+        'Tetap mengerjakan tugas agar dapat mengejar waktu tidur yang teratur',
+      point: {
+        kovva: 5,
+        mova: 5,
+      },
+    },
+    {
+      answer:
+        'Istirahat sejenak agar bisa bangun dengan kondisi yang lebih segar dan mengerjakan tugas dengan lebih baik',
+      point: {
+        sylas: 5,
+        ozirron: 5,
+      },
+    },
+    {
+      answer: 'Tidak mengerjakan tugasnya',
+      point: {
+        odra: 5,
+      },
+    },
+  ],
+  [
+    {
+      answer:
+        'Aku tidak masalah, aku tetap akan bisa berkembang di fakultas ini',
+      point: {
+        mova: 5,
+        sylas: 5,
+        odra: 5,
+      },
+    },
+    {
+      answer:
+        'Aku merasa tidak puas, tetapi tidak apa-apa karena aku masih berhasil masuk ke ITB',
+      point: {
+        ozirron: 5,
+      },
+    },
+    {
+      answer:
+        'Aku merasa tidak puas dan kemungkinan akan mengundurkan diri setelah TPB berakhir',
+      point: {
+        kovva: 5,
+      },
+    },
+  ],
+  [
+    {
+      answer: 'Ya, tentunya',
+      point: {
+        mova: 5,
+        odra: 5,
+      },
+    },
+    {
+      answer: 'Tidak, aku akan membiarkan orang lain saja',
+      point: {
+        kovva: 5,
+        ozirron: 5,
+        sylas: 5,
+      },
+    },
+  ],
+  [
+    {
+      answer: 'Kesekretariatan/kebendaharaan',
+      point: {
+        odra: 5,
+      },
+    },
+    {
+      answer: 'Akademik dan keilmuan',
+      point: {
+        kovva: 5,
+        ozirron: 5,
+      },
+    },
+    {
+      answer: 'Kesejahteraan anggota',
+      point: {
+        sylas: 5,
+      },
+    },
+    {
+      answer: 'Kreatif dan pensuasanaan',
+      point: {
+        sylas: 5,
+      },
+    },
+    {
+      answer: 'Manajemen sumber daya manusia',
+      point: {
+        kovva: 5,
+      },
+    },
+    {
+      answer: 'Relasi/hubungan masyarakat',
+      point: {
+        mova: 5,
+      },
+    },
+    {
+      answer: 'Acara',
+      point: {
+        ozirron: 5,
+      },
+    },
+  ],
+  [
+    {
+      answer: 'Ya',
+      point: {
+        kovva: 5,
+        mova: 5,
+        sylas: 5,
+        ozirron: 5,
+      },
+    },
+    {
+      answer: 'Tidak',
+      point: {
+        odra: 5,
+      },
+    },
+  ],
+];
+
+export { FirstSectionQuestions, FirstSectionAnswers };
