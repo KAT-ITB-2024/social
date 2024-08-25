8c1c1b886b0222ee45b7c12d514d4aabd4efaf25
Alexander Jason <alexanderjason526@gmail.com>
2024-08-26 03:17:02 +0700

    fix: chat modal styling

--- src/components/chat/ConfirmationModal.tsx
+++ src/components/chat/ConfirmationModal.tsx
@@ -1,6 +1,6 @@
-"use client"
+'use client';
 
-import React, { useState } from 'react'
+import React from 'react';
 import {
   AlertDialog,
   AlertDialogAction,
@@ -9,53 +9,55 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-} from "@/components/ui/alert-dialog"
-import Image from 'next/image'
-import Ombak from 'public/images/chat/Ombak.png'
+} from '@/components/ui/alert-dialog';
+import Image from 'next/image';
+import Ombak from 'public/images/chat/Ombak.png';
 
 const ConfirmationModal = ({
   title,
   description,
-  buttonLabel1,
-  buttonLabel2,
-}:{
-  title: string
-  description?: string
-  buttonLabel1: string
-  buttonLabel2?: string
+  buttonLabelConfirm,
+  buttonLabelCancel = 'Batal',
+  isOpen = true,
+  setIsOpen,
+}: {
+  title: string;
+  description?: string;
+  buttonLabelConfirm: string;
+  buttonLabelCancel?: string;
+  isOpen?: boolean;
+  setIsOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
-  const [isOpen, setIsOpen] = useState<boolean>(true)
-
   return (
     <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
       {/* Ukuran, Warna, Typography Mengikuti Figma, Tidak Bisa Rounded :( */}
-      <AlertDialogContent className='w-[343px] rounded-[32px] border-none bg-blue-600 text-white flex flex-col items-center shadow-blue-xl overflow-hidden'>
+      <AlertDialogContent className="w-[343px] rounded-[32px] border-none bg-blue-600 text-white flex flex-col items-center shadow-blue-xl overflow-hidden">
         <AlertDialogHeader>
-          <AlertDialogTitle className='text-center font-subheading text-2xl uppercase'>
+          <AlertDialogTitle className="text-center font-subheading text-2xl uppercase">
             {title}
           </AlertDialogTitle>
         </AlertDialogHeader>
         <AlertDialogFooter>
-          <div className='flex flex-col items-center gap-y-4 space-y-4'>
-            <div className='flex items-center gap-x-4'>
-              <AlertDialogCancel className='bg-pink-300 hover:bg-pink-400 border-white text-neutral-50 hover:text-neutral-100 rounded-[13px] font-body focus-visible:ring-transparent w-[120px] text-xl'>
-                {buttonLabel1}
+          <div className="flex flex-col items-center gap-y-4">
+            <div className="flex items-center gap-x-4">
+              <AlertDialogCancel className="bg-pink-300 hover:bg-pink-400 border-white text-neutral-50 hover:text-neutral-100 rounded-[13px] font-body focus-visible:ring-transparent w-[120px] text-lg">
+                {buttonLabelConfirm}
               </AlertDialogCancel>
-              <AlertDialogAction className='bg-neutral-50 border-blue-200 text-blue-600 hover:bg-neutral-100 w-[120px] rounded-[13px]'>
-                {buttonLabel2}
+              <AlertDialogAction className="bg-neutral-50 border-blue-200 border-[2px] text-blue-600 hover:bg-neutral-200 w-[120px] rounded-[13px] font-body text-lg">
+                {buttonLabelCancel}
               </AlertDialogAction>
             </div>
-            <p>{description}</p>
+            <p className="text-center text-md px-8">{description}</p>
           </div>
         </AlertDialogFooter>
-        <Image 
+        <Image
           src={Ombak}
-          alt='Ombak'
-          className='absolute bottom-0 w-full translate-y-[150px] -z-20 rounded-[10px] opacity-50'
+          alt="Ombak"
+          className="absolute bottom-0 w-full translate-y-[150px] -z-20 rounded-[10px] opacity-50"
         />
       </AlertDialogContent>
     </AlertDialog>
-  )
-}
+  );
+};
 
-export default ConfirmationModal
+export default ConfirmationModal;
--- src/components/chat/InformationModal.tsx
+++ src/components/chat/InformationModal.tsx
@@ -1,6 +1,6 @@
-"use client"
+'use client';
 
-import React, { useState } from 'react'
+import React from 'react';
 import {
   AlertDialog,
   AlertDialogCancel,
@@ -8,46 +8,48 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-} from "@/components/ui/alert-dialog"
-import Image from 'next/image'
-import Ombak from 'public/images/chat/Ombak.png'
+} from '@/components/ui/alert-dialog';
+import Image from 'next/image';
+import Ombak from 'public/images/chat/Ombak.png';
 
 const InformationModal = ({
   title,
   description,
-  buttonLabel
-}:{
-  title: string
-  description: string
-  buttonLabel: string
+  buttonLabel,
+  isOpen = true,
+  setIsOpen,
+}: {
+  title: string;
+  description: string;
+  buttonLabel: string;
+  isOpen?: boolean;
+  setIsOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
-  const [isOpen, setIsOpen] = useState<boolean>(true)
-
   return (
     <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
       {/* Ukuran, Warna, Typography Mengikuti Figma, Tidak Bisa Rounded :( */}
-      <AlertDialogContent className='w-[343px] rounded-[32px] border-none bg-blue-600 text-white flex flex-col items-center shadow-blue-xl overflow-hidden'>
+      <AlertDialogContent className="w-[343px] rounded-[32px] border-none bg-blue-600 text-white flex flex-col items-center shadow-blue-xl overflow-hidden">
         <AlertDialogHeader>
-          <AlertDialogTitle className='text-center font-subheading text-2xl uppercase'>
+          <AlertDialogTitle className="text-center font-subheading text-2xl uppercase">
             {title}
           </AlertDialogTitle>
         </AlertDialogHeader>
         <AlertDialogFooter>
-          <div className='flex flex-col items-center gap-y-4 space-y-4'>
-            <AlertDialogCancel className='bg-pink-300 hover:bg-pink-400 border-white text-neutral-50 hover:text-neutral-100 rounded-[13px] font-body focus-visible:ring-transparent w-[192px] text-xl'>
+          <div className="flex flex-col items-center gap-y-4 space-y-4">
+            <AlertDialogCancel className="bg-pink-300 hover:bg-pink-400 border-white text-neutral-50 hover:text-neutral-100 rounded-[13px] font-body focus-visible:ring-transparent w-[192px] text-xl">
               {buttonLabel}
             </AlertDialogCancel>
             <p>{description}</p>
           </div>
         </AlertDialogFooter>
-        <Image 
+        <Image
           src={Ombak}
-          alt='Ombak'
-          className='absolute bottom-0 w-full translate-y-[150px] -z-20 rounded-[10px] opacity-50'
+          alt="Ombak"
+          className="absolute bottom-0 w-full translate-y-[150px] -z-20 rounded-[10px] opacity-50"
         />
       </AlertDialogContent>
     </AlertDialog>
-  )
-}
+  );
+};
 
-export default InformationModal
+export default InformationModal;
