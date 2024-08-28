67ce5d3e7f8a39ef11cc100eab1acdc81a91a601
firzatullah al ghiffari <firzatullahalghiffari@gmail.com>
2024-08-28 19:50:35 +0700

    feat: add useRouter at MenuButton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@
 // import { api } from "~/trpc/server";
 // import { Button } from '@/components/ui/button';
 import { MenuButton } from '../components/home/MenuButton';
+import Image from 'next/image';
 
 export default async function Home() {
   return (
     <main
-      className="flex min-h-screen flex-col items-center justify-center"
+      className="flex min-h-screen flex-col items-center"
       style={{
         backgroundImage: "url('/components/bg-home.png')",
         backgroundSize: 'cover',
@@ -15,16 +16,22 @@ export default async function Home() {
         backgroundRepeat: 'no-repeat',
       }}
     >
-      <div className="flex flex-row mb-10">
+      <div className="flex flex-row mb-5">
         <MenuButton label="Chat" variant="Chat" />
         <MenuButton label="Leaderboard" variant="Leaderboard" />
         <MenuButton label="OSKM MBTI" variant="OSKM MBTI" />
         <MenuButton label="Class Selection" variant="Class Selection" />
       </div>
 
-      <div className="flex container border-solid border-2 border-[#C5FFF3] shadow-md shadow-turquoise-200/50 bg-turquoise-100 rounded-xl w-96 p-1">
+      <div className="flex container border-solid border-2 border-turquoise-100 shadow-[4px_4px_6px_rgba(255,105,180,0.75)] shadow-turquoise-200/50 bg-turquoise-100 rounded-xl w-96 p-1">
         <div className="flex flex-row w-full">
-          <img src="/components/coin.png" className="w-16 h-auto" />
+          <Image
+            src="/components/coin.png"
+            className="w-16 h-auto"
+            width={62}
+            height={62}
+            alt="coin"
+          />
           <h5 className=" ml-2 mt-1 text-blue-600">
             {' '}
             Your coins <br /> <p className="sh3 text-turquoise-400">9999999</p>
