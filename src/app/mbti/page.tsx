9f41c9a3012b25255faa725a7167f757a693dfff
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-08-30 17:00:39 +0700

    feat: add mbti files

---
+++ src/app/mbti/page.tsx
@@ -0,0 +1,46 @@
+'use client';
+import Image from 'next/image';
+import { useState } from 'react';
+import FirstSection from '~/components/mbti/FirstSection';
+import Landing from '~/components/mbti/Landing';
+import { Button } from '~/components/ui/button';
+import MbtiResult from '~/components/mbti/MbtiResult';
+import { type State } from '~/components/mbti/QnAData';
+import LoadingMBTI from '~/components/mbti/LoadingMBTI';
+
+export default function MbtiPage() {
+  const [state, setState] = useState<State>('not started');
+  const [mostType, setMostType] = useState('');
+
+  let bgImgUrl = "url('/images/mbti/bg-mbti-landing.png')";
+  if (state === 'started') {
+    bgImgUrl = `url('/images/mbti/bg-mbti-firstSection.png')`;
+  }
+  if (state === 'loading') {
+    bgImgUrl = `url('/images/mbti/bg-mbti-loading.png')`;
+  }
+  if (state === 'finished') {
+    bgImgUrl = `url('/images/mbti/bg-mbti-${mostType}.png')`;
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div
+        className="fixed-width-container flex flex-col items-center"
+        style={{
+          backgroundImage: bgImgUrl,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: '100% 100%',
+          backgroundPosition: 'center',
+        }}
+      >
+        {state == 'not started' && <Landing onStart={setState} />}
+        {state == 'started' && (
+          <FirstSection onFinished={setState} setMostType={setMostType} />
+        )}
+        {state == 'loading' && <LoadingMBTI onFinish={setState} />}
+        {state == 'finished' && <MbtiResult type={mostType} />}
+      </div>
+    </main>
+  );
+}
---
+++ src/components/mbti/MbtiResult.tsx
@@ -0,0 +1,120 @@
+import Image from 'next/image';
+import { Button } from '../ui/button';
+import { ResultsData } from './ResultsData';
+import {
+  LineShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  LineIcon,
+  TwitterIcon,
+  WhatsappIcon,
+  XIcon,
+  FacebookShareButton,
+  FacebookIcon,
+  TelegramShareButton,
+  TelegramIcon,
+} from 'react-share';
+import { useState } from 'react';
+interface MbtiResultProps {
+  type: string;
+}
+
+export default function MbtiResult({ type }: MbtiResultProps) {
+  const [saved, setSaved] = useState(false);
+  const result = ResultsData.find((obj) => obj.type === type);
+  const capitalizedType = type.charAt(0).toUpperCase() + type.slice(1);
+  const share_url = 'app.oskmitb.com/personality';
+  const share_content = `Aku mendapatkan ${capitalizedType} di OSKM MBTI!\n${result?.desc_string}\nAyo coba cek personality kalian di\n`;
+  function handleSave() {
+    const link = document.createElement('a');
+    link.href = `/images/mbti/${type}_download.png`;
+    link.download = `oskm-mbti.png`;
+    link.click();
+    setSaved(true);
+    // setTimeout(() => {
+    //   window.location.href = 'https://www.instagram.com/';
+    // }, 1000);
+  }
+
+  return (
+    <div className="px-9 flex flex-col items-center mt-24 overflow-y-auto">
+      <h2 className="text-center text-xl mt-2">
+        Yeay! we finally have the result of your test, so your MBTI is...
+      </h2>
+      <h1 className="text-pink-400 text-shadow-pink-md mt-6">
+        {capitalizedType}
+      </h1>
+      <Image
+        src={`/images/mbti/char-mbti-${type}.png`}
+        width={350}
+        height={350}
+        alt={type}
+      />
+      <h3 className="text-blue-500 text-center text-xl text-shadow-white-sm mt-2">
+        Let&rsquo;s check out what personalities does your character has!
+      </h3>
+      <div
+        className="mt-3 bg-purple bg-opacity-75 py-5 px-6 text-center rounded-xl flex flex-col 
+      items-center w-full overflow-y-auto h-44 no-scrollbar"
+      >
+        <h1 className="text-2xl text-yellow text-shadow-neutral-sm">
+          Personalities
+        </h1>
+        {result!.personality}
+      </div>
+      <Button
+        className="mt-5 text-purple bg-yellow rounded-lg text-xl w-[40%] py-7 font-medium shadow-orange-md"
+        onClick={handleSave}
+      >
+        Save Result
+      </Button>
+      {saved && (
+        <>
+          <div className="fixed inset-0 bg-black opacity-65 z-10"></div>
+          <div className="flex flex-col items-center absolute top-1/2 left-1/2 bg-lightYellow translate-x-[-50%] translate-y-[-50%] w-[95%] py-7 rounded-xl z-20">
+            <div className="relative w-[90%]">
+              <button className="absolute right-1 top-1">
+                <Image
+                  src={`/images/mbti/cross.png`}
+                  width={50}
+                  height={50}
+                  alt="cross"
+                  onClick={() => {
+                    setSaved(false);
+                  }}
+                ></Image>
+              </button>
+              <Image
+                src={`/images/mbti/${type}_download.png`}
+                width={350}
+                height={350}
+                layout="responsive"
+                alt={type}
+              />
+            </div>
+            <h1 className="text-3xl text-success-600 mt-5">
+              Successfully Saved!
+            </h1>
+            <h3 className="mt-1 text-xl text-green-950">
+              Share to your socials!
+            </h3>
+            <div className="flex justify-center gap-3 items-center w-1/2">
+              <WhatsappShareButton url={share_url} title={share_content}>
+                <WhatsappIcon size={32} round />
+              </WhatsappShareButton>
+              <TwitterShareButton url={share_url} title={share_content}>
+                <XIcon size={32} round />
+              </TwitterShareButton>
+              <TelegramShareButton url={share_url} title={share_content}>
+                <TelegramIcon size={32} round />
+              </TelegramShareButton>
+              <LineShareButton url={share_url} title={share_content}>
+                <LineIcon size={32} round />
+              </LineShareButton>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
