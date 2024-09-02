bae7566409ef709e4e9d27ec6d300cffee61ec16
Alexander Jason <alexanderjason526@gmail.com>
2024-09-02 16:12:18 +0700

    fix: profile background and sidebar name

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -11,42 +11,44 @@ export default async function ProfilePage() {
   }
   const { profilePic } = userProfile;
   return (
-    <div
-      style={{
-        backgroundImage: 'url(/images/profile/BackgroundProfile.jpeg)',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        minHeight: '100vh',
-        position: 'relative',
-      }}
-    >
-      <Image
-        src="/images/profile/wave.png"
-        alt="Wave"
-        width={300}
-        height={100}
-        className="absolute left-0 z-0 h-auto w-auto translate-y-[-60px] rotate-[10deg]"
-      />
-      <Image
-        src="/images/profile/turtle.png"
-        alt="Fish"
-        width={200}
-        height={200}
-        className="absolute top-10 z-0"
-      />
+    <main className="flex h-screen flex-col items-center justify-center">
+      <div
+        className="fixed-width-container flex flex-col p-0"
+        style={{
+          backgroundImage: 'url(/images/profile/BackgroundProfile.jpeg)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <Image
+          src="/images/profile/wave.png"
+          alt="Wave"
+          width={300}
+          height={100}
+          className="absolute left-0 z-0 h-auto w-auto translate-y-[-60px] rotate-[10deg]"
+        />
+        <Image
+          src="/images/profile/turtle.png"
+          alt="Fish"
+          width={200}
+          height={200}
+          className="absolute top-10 z-0"
+        />
 
-      <Image
-        src="/images/profile/starfish.png"
-        alt="Starfish"
-        width={250}
-        height={250}
-        className="absolute right-0 top-96 z-0"
-      />
+        <Image
+          src="/images/profile/starfish.png"
+          alt="Starfish"
+          width={250}
+          height={250}
+          className="absolute right-0 top-96 z-0"
+        />
 
-      <div className="z-1 relative">
-        <ProfileHeader profilePic={profilePic} />
-        <ProfileDetails {...userProfile} />
+        <div className="z-1 relative">
+          <ProfileHeader profilePic={profilePic} />
+          <ProfileDetails {...userProfile} />
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
