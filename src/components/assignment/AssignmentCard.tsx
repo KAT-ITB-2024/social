ae6c50ae0588a305d4043fee907e0f438b9cf3fd
ValentinoTriadi <valent122016@gmail.com>
2024-09-02 22:09:11 +0700

    fix: change date format & upload on late

--- src/components/assignment/AssignmentCard.tsx
+++ src/components/assignment/AssignmentCard.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { AssignmentSubmission } from '~/types/enums/assignment';
 import { Chip } from '../Chip';
 import { Button } from '../ui/button';
 import { Card, CardContent } from '../ui/card';
@@ -11,7 +12,7 @@ export interface AssignmentCardProps {
   id: string;
   title: string;
   deadline: Date;
-  status: string;
+  status: AssignmentSubmission;
 }
 
 export const AssignmentCard = ({
@@ -26,28 +27,31 @@ export const AssignmentCard = ({
   };
 
   return (
-    <Card className="w-full px-6 py-4 bg-card-radial border-turquoise-300 border-2 rounded-[12px]">
-      <CardContent className="p-0 flex justify-between items-center">
-        <div className="flex gap-[30px] items-center w-full">
-          <div className="w-full flex flex-col gap-2">
-            <h1 className="font-subheading font-bold text-b3 text-blue-500">
+    <Card className="bg-card-radial w-full rounded-[12px] border-2 border-turquoise-300 px-6 py-4">
+      <CardContent className="flex items-center justify-between p-0">
+        <div className="flex w-full items-center gap-[30px]">
+          <div className="flex w-full flex-col gap-2">
+            <h1 className="font-subheading text-b3 font-bold text-blue-500">
               {title}
             </h1>
-            <h2 className="font-subheading font-normal text-b4 text-blue-500">
+            <h2 className="font-subheading text-b4 font-normal text-blue-500">
               <b>Deadline : </b>{' '}
-              {format(deadline, 'PPPP', {
+              {format(deadline, 'PP - HH:mm', {
                 locale: idLocale,
               })}
             </h2>
-            {status === 'KUMPUL' && <Chip label="terkumpul" variant="GREEN" />}
-            {status === 'BELUM KUMPUL' && !isLate() && (
+            {status === AssignmentSubmission.TERKUMPUL && (
+              <Chip label="terkumpul" variant="GREEN" />
+            )}
+            {status === AssignmentSubmission.BELUM_KUMPUL && !isLate() && (
               <Chip label="belum kumpul" variant="YELLOW" />
             )}
-            {status === 'BELUM KUMPUL' && isLate() && (
+            {((status === AssignmentSubmission.BELUM_KUMPUL && isLate()) ||
+              status === AssignmentSubmission.TERLAMBAT) && (
               <Chip label="terlambat" variant="RED" />
             )}
           </div>
-          <Button variant={'blue'} className="py-2 px-5">
+          <Button variant={'blue'} className="px-5 py-2">
             <Link href={`/assignment/${id}`}>Open</Link>
           </Button>
         </div>
