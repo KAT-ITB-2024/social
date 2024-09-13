2ccc1f37c9f36cb5d8eecfab70bbae1446dd68b6
Kevin Sebastian <116342133+kevinssheva@users.noreply.github.com>
2024-09-13 22:18:37 +0700

    fix: sidebar and lembaga page

--- src/app/(with-desktop)/lembaga/mobile-view.tsx
+++ src/app/(with-desktop)/lembaga/mobile-view.tsx
@@ -44,19 +44,19 @@ export default function MobileView({ lembaga }: { lembaga: Lembaga }) {
       {/* Main Section */}
       <section className="flex h-[65vh] w-full flex-col items-center justify-between">
         <div className="flex w-full flex-col items-center">
-          <p className="text-center font-heading text-4xl text-orange-500 drop-shadow-orange-shadow-lg">
+          <p className="line-clamp-2 text-center font-heading text-4xl text-orange-500 drop-shadow-orange-shadow-lg">
             {name}
           </p>
           <p className="font-body text-lg text-pink-300">
             {visitorCount} Pengunjung
           </p>
         </div>
-        <div className="flex h-fit w-3/4 flex-col items-center gap-1">
+        <div className="relative flex aspect-square w-3/4 flex-col items-center justify-center gap-1 pb-8">
           <Image
             fill
             src="/images/lembaga/bubble.png"
             alt="bubble"
-            className="-z-10 object-contain p-10"
+            className="h-full w-full object-contain"
           />
           <p className="font-subheading text-lg font-bold text-orange-500">
             Kode Kunjungan
@@ -77,7 +77,7 @@ export default function MobileView({ lembaga }: { lembaga: Lembaga }) {
           <Button
             asChild
             variant="outline"
-            className="w-full min-w-fit border-2 border-orange-400 bg-transparent text-orange-400 drop-shadow-orange-shadow-lg hover:bg-white hover:text-orange-400"
+            className="w-full min-w-fit border-2 border-orange-400 bg-transparent font-semibold text-orange-400 drop-shadow-orange-shadow-lg hover:bg-white hover:text-orange-400"
             disabled={isRefreshing}
           >
             <Link href="/lembaga/grant-coins">Lihat Pengunjung</Link>
